1afe18d7d5444aeaa1f62a109fe10628f179b348
Guy Eisenberg <guyeisenberg@guyeisenberg.com>
2025-09-28 14:22:13 +0300

    Hide nav menu button for now

--- app/@navbar/Navbar.tsx
+++ app/@navbar/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar({
     <nav className="flex w-full items-center justify-between border-b pr-4">
       <div className="box-content flex w-[56px] p-4 pr-0">
         <button>
-          <MenuIcon className="size-8" />
+          <MenuIcon className="size-8 opacity-0" />
         </button>
       </div>
       <BrandIcon className="size-8" />
